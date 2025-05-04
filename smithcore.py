672e8b28c34ea8e4342d8351321e6df1089f700e
Smithcore <faizanuber6@gmail.com>
2025-05-04 18:18:00 +0930

    Update smithcore.py

--- smithcore.py
+++ smithcore.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from hybrid_brain import smith_brain
 from flask_cors import CORS
+import os  # <-- THIS WAS MISSING
 
 app = Flask(__name__)
 CORS(app)
@@ -46,5 +47,3 @@ def blackbox():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
-
-
